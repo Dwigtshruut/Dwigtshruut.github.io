19c0a2320567e35f5688284ca97ded12b26d21f1
Dwigtshruut <litomotolitomoto@gmail.com>
2021-07-17 22:24:02 +0545

    sample

--- Practice websites/index.js
+++ Practice websites/index.js
@@ -1,4 +1,5 @@
  let changes = document.querySelector(".head").value;
+ alert(changes);
  document.addEventListener("DOMContentLoaded", function() {
     document.querySelector("form").onsubmit= function(){
         document.querySelector("header").innerHTML= changes;
