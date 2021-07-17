ebcc08d06af2e6cf72494a6a1362d3f9b2c66d86
Dwigtshruut <litomotolitomoto@gmail.com>
2021-07-17 22:51:46 +0545

    This is a s last try

--- Practice websites/index.js
+++ Practice websites/index.js
@@ -1,7 +1,6 @@
- let changes = document.querySelector(".head").value;
- alert(changes);
- document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("form").onsubmit= function(){
-        document.querySelector("header").innerHTML= changes;
-    }
- });
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector("form").onsubmit = function(){
+        let header = document.querySelector(".head").value;
+        document.querySelector("header").innerHTML = header;
+    };
+});
