7bf9d81d693897971834a571a84c084c3823b831
Dwigtshruut <litomotolitomoto@gmail.com>
2022-03-05 21:43:41 +0545

    Merge branch 'master' of https://github.com/Dwigtshruut/Dwigtshruut.github.io git commit -m "created Two brances now making it one"

--- Practice websites/index.js
+++
@@ -1,4 +0,0 @@
-document.querySelector("form").onsubmit = function(){
-        let header = document.querySelector(".head").value;
-        document.querySelector("header").innerHTML = header;
-    };
